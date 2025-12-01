254b360e61bd4e4f9a7a9f449e92db7615cc9d18
afsalllll <shajixd71@gmail.com>
2025-12-01 22:57:37 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+API_ID = 33300320  # your api id
+API_HASH = "49a2dd1e85164d95e77d9952a006c36b"
+BOT_TOKEN = "8541239294:AAEsziaD5j-S30G-RhJt0iQDgq98RvX1tDM"
+
+# Auto-forward settings
+SOURCE_CHAT = -1003438376071     # from where messages come
+
+# Target groups/channels to forward messages
+TARGET_CHATS = [
+    -1002307923698,
+    -1002738151866,
+    -1002330208229,
+    -1002583103466,
+    -1002391657522,
+    -1002306956966,
+    -1002465666324,
+    -1003445716234,
+    -1002508347924
+]
+
+# Scheduled message
+SCHEDULE_TEXT = "⏰ This is your scheduled message!"
+SCHEDULE_INTERVAL = 3600  # 1 hour = 3600 seconds (change if needed) 
